1d657a118fe8de7f676fb85f7f7dee2f2ef761c4
lazyjsh03 <lazy.jsh03@gmail.com>
2025-11-10 15:10:50 +0900

    first commit: 프로젝트 생성

---
+++ src/main/java/jsh/project/minisecurity/MinisecurityApplication.java
@@ -0,0 +1,13 @@
+package jsh.project.minisecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinisecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinisecurityApplication.class, args);
+	}
+
+}
