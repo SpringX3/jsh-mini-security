c7019c8b55a8cd1702d391fe5d1fe115010c6694
lazyjsh03 <lazy.jsh03@gmail.com>
2025-11-11 14:15:48 +0900

    feat: AuthorizationFilter을 스프링 빈에 등록

--- src/main/java/jsh/project/minisecurity/config/SecurityFilterConfig.java
+++ src/main/java/jsh/project/minisecurity/config/SecurityFilterConfig.java
@@ -2,6 +2,7 @@ package jsh.project.minisecurity.config;
 
 import jsh.project.minisecurity.security.authentication.ProviderManager;
 import jsh.project.minisecurity.security.filter.AuthenticationFilter;
+import jsh.project.minisecurity.security.filter.AuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +20,15 @@ public class SecurityFilterConfig {
         registration.setFilter(new AuthenticationFilter(providerManager));
         registration.addUrlPatterns("/*");
         registration.setOrder(1);
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean<AuthorizationFilter> authorizationFilter() {
+        FilterRegistrationBean<AuthorizationFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new AuthorizationFilter());
+        registration.addUrlPatterns("/*");
+        registration.setOrder(2);   // AuthenticationFilter 다음 순서
         return registration;
     }
 }
