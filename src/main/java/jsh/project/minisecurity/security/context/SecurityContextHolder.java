167cbff93f1af158829da738ca5fd9ff40d16950
lazyjsh03 <lazy.jsh03@gmail.com>
2025-12-02 10:48:48 +0900

    docs: 주석 추가

--- src/main/java/jsh/project/minisecurity/security/context/SecurityContextHolder.java
+++ src/main/java/jsh/project/minisecurity/security/context/SecurityContextHolder.java
@@ -2,10 +2,15 @@ package jsh.project.minisecurity.security.context;
 
 public class SecurityContextHolder {
 
+    // SecurityContext를 ThreadLocal로 저장
     private static final ThreadLocal<SecurityContext> contextHolder = new ThreadLocal<>();
 
+    // SecurityContext 조회
     public static SecurityContext getContext() {
+        // Thread에 존재하는 SecurityContext 조회
         SecurityContext context = contextHolder.get();
+
+        // Thread에 없으면 생성
         if (context == null) {
             context = new SecurityContext();
             contextHolder.set(context);
@@ -18,6 +23,7 @@ public class SecurityContextHolder {
         contextHolder.set(context);
     }
 
+    // SecurityContext Clear
     public static void clearContext() {
         contextHolder.remove();
     }
